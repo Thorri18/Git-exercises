958a6947561b167aa187b819d977599471bb7720
Thorri18 <torri97@hotmail.com>
2018-09-04 18:21:39 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,14 @@
+# Input of integer.
+n = int(input("Enter the length of the sequence: "))
+# Start off with 0+1+2 and then remove the first number in the equation and add the new one at the end of it.
+
+if n < 0:
+    print("Invalid input")
+elif n < 3:
+    print(n)
+else:
+    n >= 6
+    n = n + 3 + 2
+print(n)
+
+# Print until the answer reaches the triple digits.

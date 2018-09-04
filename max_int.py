42507cfc7b7f765998b9c55096a5c035405c9b8e
Thorri18 <torri97@hotmail.com>
2018-09-04 10:56:26 +0000

    Compelete.

--- max_int.py
+++ max_int.py
@@ -1,6 +1,14 @@
 # Input integer
+num_int = int(input("Input a number: "))
 
+max_int = num_int
 # When input is above 0, ask for another
 # Stop when negative integer is input
+while num_int >= 0:
+    num_int = max_int
+    num_int = int(input("Input a number: "))
+    if num_int < 0:
+        break
 
 # Print out input
+print("The maximum is", max_int)
